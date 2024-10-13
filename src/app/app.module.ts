13bc72269469e816257327aadd617f239e5430af
pedroMT7 <126702127+pedroMT7@users.noreply.github.com>
2024-10-13 03:25:02 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';  
+import { BrowserModule } from '@angular/platform-browser';  
+import { FormsModule } from '@angular/forms';  
+import { AppRoutingModule } from './app-routing.module';  
+import { AppComponent } from './app.component';  
+import { HomeComponent } from './home/home.component';  
+import { LoginComponent } from './login/login.component';  
+import { CadastrarComponent } from './cadastrar/cadastrar.component';  
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';  
+
+@NgModule({  
+  declarations: [  
+    AppComponent,  
+    HomeComponent,  
+    LoginComponent,  
+    CadastrarComponent,  
+    PageNotFoundComponent,  
+  ],  
+  imports: [  
+    BrowserModule,  
+    AppRoutingModule,  
+    FormsModule,  
+  ],  
+  providers: [],  
+  bootstrap: [AppComponent],  
+})  
+export class AppModule { }
